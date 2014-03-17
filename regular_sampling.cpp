550cf79bf1f99247e3b1a926f24fed8c1604cf4a
jimmy on yoshi <migichen@gmail.com>
2014-03-17 17:41:27 -0400

    Probe Filter
    	modified:   regular_sampling.cpp

--- regular_sampling.cpp
+++ regular_sampling.cpp
@@ -27,10 +27,18 @@
 #include "vtkNew.h"
 #include "vtkSmartPointer.h"
 #include "vtkMultiBlockDataSet.h"
+#include "vtkMultiPieceDataSet.h"
 #include "vtkXMLMultiBlockDataWriter.h"
 #include "vtkZLibDataCompressor.h"
 // interpolate
 #include "vtkInterpolatedVelocityField.h"
+#include "vtkImageData.h"
+#include "vtkProbeFilter.h"
+#include "vtkPointData.h"
+#include "vtkFloatArray.h"
+#include "vtkDoubleArray.h"
+#include "vtkCompositeDataProbeFilter.h"
+#include "vtkXMLImageDataWriter.h"
 
 using namespace std;
 
@@ -40,8 +48,7 @@ vtkLineWidget *lineWidget;
 vtkRenderWindow *renWin;
 vtkPolyData *seeds ;
 
-
-vtkSmartPointer<vtkMultiBlockDataSet> load_list(char *list_fname)
+vtkSmartPointer<vtkMultiPieceDataSet> load_list(char *list_fname)
 {
 	//char list_filename[1024];
 	//sprintf(list_filename, "%s/list", DATA_PATH);
@@ -51,7 +58,7 @@ vtkSmartPointer<vtkMultiBlockDataSet> load_list(char *list_fname)
 
 	int blocks = atoi(s);
 	int i;
-	vtkSmartPointer<vtkMultiBlockDataSet> mb = vtkMultiBlockDataSet::New();
+	vtkSmartPointer<vtkMultiPieceDataSet> mb = vtkMultiPieceDataSet::New();
 
 	for (i=0; i<blocks; i++)
 	{
@@ -73,8 +80,8 @@ vtkSmartPointer<vtkMultiBlockDataSet> load_list(char *list_fname)
 		reader->SetAutoDetectFormat(1);
 
 	    reader->AddFunction(100); //density
-	    //reader->AddFunction(110); //pressure
-	    //reader->AddFunction(120); //temp
+	    reader->AddFunction(110); //pressure
+	    reader->AddFunction(120); //temp
 	    //reader->AddFunction(130); //enthalpy
 	    //reader->AddFunction(140); //internal energy
 	    //reader->AddFunction(144); //kinetic energy
@@ -96,13 +103,13 @@ vtkSmartPointer<vtkMultiBlockDataSet> load_list(char *list_fname)
 
 
 
-		mb->SetBlock(i, current_data);
+		mb->SetPiece(i, current_data);
 
 	}
 	return mb;
 }
 
-float RES = .01;
+float RES = .005;
 float datamin[3] = {-0.083, -0.509, -0.509};
 float datamax[3] = {0.0914, 0.509, 0.509};
 struct float3 {float x,y,z;};
@@ -115,7 +122,7 @@ int main(int argc, char **argv)
     vtkSmartPointer<vtkDataCompressor> compressor = vtkZLibDataCompressor::New();
 
 	// load data
-	vtkSmartPointer<vtkMultiBlockDataSet> mb = load_list(argv[1]);
+	vtkSmartPointer<vtkMultiPieceDataSet> mb = load_list(argv[1]);
 
 	// resample
 	int extent[3];
@@ -124,9 +131,9 @@ int main(int argc, char **argv)
 	// set interpolator
 
 	vtkInterpolatedVelocityField *vecInterp = vtkInterpolatedVelocityField::New();
-	for (b=0; b<mb->GetNumberOfBlocks(); b++)
+	for (b=0; b<mb->GetNumberOfPieces(); b++)
 	{
-		vtkDataSet *dataset = vtkDataSet::SafeDownCast( mb->GetBlock(b) );
+		vtkDataSet *dataset = vtkDataSet::SafeDownCast( mb->GetPiece(b) );
 		double *bounds = dataset->GetBounds();
 		//printf("bounds: %f %f %f %f %f %f\n", bounds[0], bounds[1], bounds[2], bounds[3], bounds[4], bounds[5], bounds[6]);
 
@@ -139,6 +146,83 @@ int main(int argc, char **argv)
 	printf("Extent: %d %d %d\n", extent[0], extent[1], extent[2]);
 	vector<float3> vec_ary(extent[0]*extent[1]*extent[2]);
 
+#if 1
+
+	vtkSmartPointer<vtkImageData> samplingArray = vtkImageData::New();
+	//samplingArray->SetNumberOfScalarComponents(1);
+	//samplingArray->SetScalarType(VTK_FLOAT);
+	samplingArray->SetSpacing(RES, RES, RES);
+	samplingArray->SetDimensions(extent[0], extent[1], extent[2]);
+	double origin[3];
+	origin[0] = (double)datamin[0];
+	origin[1] = (double)datamin[1];
+	origin[2] = (double)datamin[2];
+	samplingArray->SetOrigin( origin );
+	samplingArray->AllocateScalars(VTK_FLOAT,1);
+	samplingArray->ComputeBounds();
+	double *bounds = samplingArray->GetBounds();
+	printf("output bounds: %f %f %f %f %f %f\n", bounds[0], bounds[1], bounds[2], bounds[3], bounds[4], bounds[5]);
+	//bounds = vtkDataSet::SafeDownCast(mb->GetBlock(0))->GetBounds();
+	//printf("input bounds: %f %f %f %f %f %f\n", bounds[0], bounds[1], bounds[2], bounds[3], bounds[4], bounds[5]);
+
+	vtkSmartPointer<vtkCompositeDataProbeFilter> resampler = vtkCompositeDataProbeFilter::New();
+	resampler->SetSourceData(mb);
+	resampler->SetInputData(samplingArray);
+	resampler->SpatialMatchOn();
+	resampler->Update();
+
+	vtkSmartPointer<vtkImageData> output = vtkImageData::SafeDownCast(resampler->GetOutput());
+
+	char filename[256];
+	sprintf(filename, "%s.vti", argv[1]);
+	vtkNew<vtkXMLImageDataWriter> imw;
+	imw->SetFileName(filename);
+	imw->SetDataModeToBinary();
+	imw->SetInputData(output);
+	imw->SetCompressor(compressor);
+	imw->Write();
+#if 0
+	//vtkImageData *image = vtkImageData::New();
+	//image->DeepCopy(resampler->GetImageDataOutput());
+	vtkDataArray* data = resampler->GetOutput()->GetPointData()->GetScalars();
+	vtkSmartPointer<vtkFloatArray> ary = vtkFloatArray::SafeDownCast(data);
+	//for (int i=0; i<ary->GetNumberOfTuples(); i++) {
+	//	if (ary->GetValue(i) != 0 )
+	//		printf("%f, ", ary->GetValue(i));
+	//}
+
+	char filename[256];
+	char *namebase ;
+	if (argc==2)
+		namebase = argv[1];
+	else
+		namebase = argv[2];
+
+	printf("output: %s_v.raw, %s_v.nhdr\n", namebase, namebase);
+
+	sprintf(filename, "%s.raw", namebase);
+	FILE *fp = fopen(filename, "wb");
+	fwrite(ary->GetVoidPointer(0), sizeof(float), ary->GetNumberOfTuples(), fp);
+	fclose(fp);
+
+	sprintf(filename, "%s_v.nhdr", namebase);
+	fp = fopen(filename, "wt");
+	fprintf(fp, "NRRD0004\n"
+			"type: float\n"
+			"dimension: 3\n"
+			"sizes: %d %d %d\n"
+			"encoding: raw\n"
+			"data file: %s_v.raw\n"
+			"space origin: (%f,%f,%f)\n"
+			"space directions: (%f,0,0) (0,%f,0) (0,0,%f)\n"
+			, extent[0], extent[1], extent[2],
+			namebase,
+			datamin[0], datamin[1], datamin[2],
+			RES, RES, RES);
+	fclose(fp);
+#endif
+
+#else
 	for (k=0; k<extent[2]; k++)
 	{
 		for (j=0; j<extent[1]; j++)
@@ -200,6 +284,7 @@ int main(int argc, char **argv)
 			RES, RES, RES);
 	fclose(fp);
 
+#endif
 #if 0
 	vtkNew<vtkXMLMultiBlockDataWriter> mbw;
 	mbw->SetFileName("merged.vtm");
